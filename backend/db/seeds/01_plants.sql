21a6b8983e406b741db041191e66b8b991c43646
Diana <dianalop1998@gmail.com>
2024-07-07 22:53:39 -0400

    add seeds for plant table

--- backend/db/seeds/01_plants.sql
+++ backend/db/seeds/01_plants.sql
@@ -1,4 +1,5 @@
 
-
 INSERT INTO plants (name, description, image_url) VALUES
-('Mostera Deliciosa', 'A beautiful swiss cheese like plant that likes to climb', '')
+('Mostera Deliciosa', 'A beautiful swiss cheese like plant that likes to climb', ''),
+('Fiddle Leaf FIg', 'A beautiful almond like plant that likes to sunlight and humidity', ''),
+('Antherium Magnificum', 'A beautiful anroid like plant that likes that has beautiful big leafs', '');
