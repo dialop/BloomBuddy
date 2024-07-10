0bc91b7009d480b8be519de56cb269128bf5d971
Diana <dianalop1998@gmail.com>
2024-07-09 21:56:56 -0400

    Add home page

---
+++ frontend/src/pages/Home.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Home: React.FC = () => {
+    return (
+    <main>
+        <h2>
+            Welcome to BloomBuddy
+            <p> Your one stop site all things plants </p>
+        </h2>
+    </main>
+)
+
+}
+
+
+export default Home; 
