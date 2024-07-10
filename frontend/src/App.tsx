18cb084672214c39b1dfb41114966a9511599810
Diana <dianalop1998@gmail.com>
2024-07-09 21:59:16 -0400

    update app.js and delete function and add const to App:

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import Header from "./components/Header"
-import Footer from "./components/Footer"
+import Header from "./components/common/Header"
+import Footer from "./components/common/Footer"
 import Home from "./pages/Home"
 
-function App: React.FC = () => {
+const App: React.FC = () => {
 
   return (
     <div>
