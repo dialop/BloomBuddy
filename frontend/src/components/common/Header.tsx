c8193cef3a9099aceb4098e9f05532bbc94dd5a4
Diana <dianalop1998@gmail.com>
2024-07-26 23:45:42 -0400

    remove tittle on header

--- frontend/src/components/common/Header.tsx
+++ frontend/src/components/common/Header.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const Header: React.FC = () => {
     return (
         <header>
-            <h1> BloomBuddy Collection</h1>
+            <h1> BloomBuddy </h1>
         </header>
     )
 }
