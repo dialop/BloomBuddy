b5566ed95dc004f4258b4c3ac67ca063008ff24c
Diana <dianalop1998@gmail.com>
2024-08-13 22:52:46 -0400

    improve spacing and layout

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,18 +1,18 @@
 import React from 'react';
 import './App.css';
-import Header from "./components/common/Header";
-import Footer from "./components/common/Footer";
-import Home from "./pages/Home";
+import Header from './components/common/Header';
+import Footer from './components/common/Footer';
+import Home from './pages/Home';
 
 const App: React.FC = () => {
   return (
     <div className="App flex flex-col min-h-screen">
-    <Header />
-    <main className="flex-grow">
-      <Home />
-    </main>
-    <Footer />
-  </div>
+      <Header />
+      <main className="flex-grow">
+        <Home />
+      </main>
+      <Footer />
+    </div>
   );
 };
 
