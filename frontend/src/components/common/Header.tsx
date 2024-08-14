8728e2edc9d97a495d09cd848610c699ef99f5b1
Diana <dianalop1998@gmail.com>
2024-08-13 22:54:49 -0400

    add header navigation bar layout

--- frontend/src/components/common/Header.tsx
+++ frontend/src/components/common/Header.tsx
@@ -2,7 +2,8 @@ import React from "react";
 
 const Header: React.FC = () => {
     return (
-        <header>
+        <header className="bg-blue-500 text-white p-4 shadow-md">
+            <div className="container mx-auto flex justify-between items-container"></div>
             <h1> BloomBuddy </h1>
         </header>
     )
