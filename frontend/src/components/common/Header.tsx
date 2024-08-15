604c1df1f62e37e78e27c70948f65aeffd7e3787
Diana <dianalop1998@gmail.com>
2024-08-14 22:34:25 -0400

    Remove paragraph

--- frontend/src/components/common/Header.tsx
+++ frontend/src/components/common/Header.tsx
@@ -7,7 +7,7 @@ const Header: React.FC = () => {
             <div className="md:w-1/2">
                 <p className="font-bold text-sm uppercase">Welcome to BloomBuddy</p>
                 <button className="mt-4 bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
-                    Learn More
+                    
                 </button>
             </div>
             <h1> BloomBuddy </h1>
